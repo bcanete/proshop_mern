d1c2fc4944887f2b06b45eb65c228aa0717be136
bcanete <benedickcanete@gmail.com>
2021-03-22 08:23:21 +0800

    order screen done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,37 +4,54 @@ import Order from '../models/orderModel.js'
 // @desc create new order
 // @route POST /api/orders
 // @access Private
-const addOrderItems = asyncHandler(async ( req , res )=> {
+const addOrderItems = asyncHandler(async (req, res) => {
     const {
-        orderItems, 
-        shippingAddress, 
-        paymentMethod, 
-        itemsPrice, 
-        taxPrice, 
-        shippingPrice, 
-        totalPrice
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
     } = req.body
-
-    if(orderItems && orderItems.length ===0) {
-        res.status(400)
-        throw new Error('No order items')
-        return
+  
+    if (orderItems && orderItems.length === 0) {
+      res.status(400)
+      throw new Error('No order items')
+      return
     } else {
-        const order = new Order({
-            orderItems, 
-            user: req.user._id,
-            shippingAddress, 
-            paymentMethod, 
-            itemsPrice, 
-            taxPrice, 
-            shippingPrice, 
-            totalPrice
-        })
-
-        const createdOrder = await order.save()
-
-        res.status(201).json(createdOrder)
+      const order = new Order({
+        orderItems,
+        user: req.user._id,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+      })
+  
+      const createdOrder = await order.save()
+  
+      res.status(201).json(createdOrder)
     }
 })  
 
-export { addOrderItems }
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    )
+  
+    if (order) {
+      res.json(order)
+    } else {
+      res.status(404)
+      throw new Error('Order not found')
+    }
+})
+
+export { addOrderItems, getOrderById }
--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -2,51 +2,91 @@ import axios from 'axios'
 import { 
     ORDER_CREATE_FAIL, 
     ORDER_CREATE_REQUEST, 
-    ORDER_CREATE_SUCCESS 
+    ORDER_CREATE_SUCCESS, 
+    ORDER_DETAILS_FAIL,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS
  } from "../constants/orderConstants"
 import { USER_LOGIN_SUCCESS } from '../constants/userConstants'
+import { logout } from './userActions'
 
 export const createOrder = (order) => async (dispatch, getState) => {
-    try {
-      dispatch({
-        type: ORDER_CREATE_REQUEST,
-      })
-  
-      const {
-        userLogin: { userInfo },
-      } = getState()
-  
-      const config = {
-        headers: {
-          Authorization: `Bearer ${userInfo.token}`,
-        },
-      }
-  
-      const { data } = await axios.post(`/api/orders`, order, config)
-  
-      dispatch({
-        type: ORDER_CREATE_SUCCESS,
-        payload: data,
-      })
+  try {
+    dispatch({
+      type: ORDER_CREATE_REQUEST,
+    })
 
-      dispatch({
-        type: USER_LOGIN_SUCCESS,
-        payload: data,
-      })
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
-      localStorage.setItem('userInfo', JSON.stringify(data))
-      
-    } catch (error) {
-      const message =
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message
-      if (message === 'Not authorized, token failed') {
-        
-      }
-      dispatch({
-        type: ORDER_CREATE_FAIL,
-        payload:message
-      })
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
     }
+
+    const { data } = await axios.post(`/api/orders`, order, config)
+
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data,
+    })
+    dispatch({
+      // type: CART_CLEAR_ITEMS,
+      payload: data,
+    })
+    localStorage.removeItem('cartItems')
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload: message,
+    })
+  }
+}
+
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/orders/${id}`, config)
+
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload: message,
+    })
+  }
 }
